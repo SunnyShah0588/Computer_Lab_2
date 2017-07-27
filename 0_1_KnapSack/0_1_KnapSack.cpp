c430635720d18423d20eef67447118db7a70e549
SUNNY SHAH <sunnyshah0588@gmail.com>
2017-07-27 15:47:45 +0530

    Add files via upload

---
+++ 0_1_KnapSack/0_1_KnapSack.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+
+	int upperBound(){
+	
+	                        
+	
+	
+	
+	
+	                  
+	
+	}
+	
+	int main(){
+	
+	
+	      int numberOfItems;
+	      int sackWeight;
+	      
+	      
+	      cout<<"ENTER THE NUMBER OF ITEMS"<<endl;
+	      cin>>numberOfItems;
+	      cout<<"ENTER THE WEIGHT OF SACK"<<endl;
+	      cin>>sackWeight;
+	      
+	      int items[2][numberOfItems];
+	      
+	    
+	      
+	            for(int j = 0; j < numberOfItems; j++ ){
+	            
+	                cout<<"ENTER THE WEIGHT OF THE "<< j+1 << " ITEM"<<endl;
+	                cin>>items[0][j];
+	                cout<<"ENTER THE PROFIT OF THE "<< j+1 << " ITEM"<<endl;
+	                cin>>items[1][j];
+	            
+	            }
+	      
+	      
+	      
+	     
+	      
+	           /* for(int j = 0; j < numberOfItems; j++ ){
+	            
+	                cout<<"THE WEIGHT "<<items[0][j]<<" & PROFIT " <<items[1][j]<<"OF THE "<< j+1 << " ITEM  "<<items[0][j]<<endl;
+	              
+	            
+	            }*/
+	            
+	       int selectedArray[numberOfItems];
+	       
+	       
+	       
+	            
+	      
+	      
+	      
+	      
+	      
+	
+	return 0;
+	
+	
+	}
